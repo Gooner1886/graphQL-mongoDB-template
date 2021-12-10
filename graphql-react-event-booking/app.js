81955cee0e1e49e63a1b87e90d31e75d17f6100f
Siddharth Sarma <siddharthgsarma5@gmail.com>
2021-12-10 11:00:27 +0530

    added types of the event in schema

--- graphql-react-event-booking/app.js
+++ graphql-react-event-booking/app.js
@@ -5,70 +5,96 @@ const { buildSchema } = require("graphql");
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
-
-// app.use(
-//   "/graphql",
-//   graphqlHTTP({
-//     schema: buildSchema(`
-//         type rootQuery {
-//             events: [String!]!
-//         }
-
-//         type rootMutations {
-//             createEvent(name: String): String
-//         }
-
-//         schema {
-//             query: rootQuery
-//             mutation: rootMutations
-//         }
-//     `),
-//     rootValue: {
-//       events: () => {
-//         return ["All Night Coding", "Premier League watchalong", "Sailing"];
-//       },
-//       createEvent: (args) => {
-//         const eventName = args.name;
-//         return eventName;
-//       },
-//     },
-//     graphiql: true,
-//   })
-// );
 
 app.use(
   "/graphql",
   graphqlHTTP({
     schema: buildSchema(`
-        type rootQuery {
-            posts: [String!]!
+        type Event {
+          _id: ID
+          name: String!
+          description: String!
+          price: Float!
+          date: String!
         }
 
-        type rootMutation {
-            createPosts(postName: String): String 
+        input EventInput {
+          name: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+
+        type rootQuery {
+            events: [Event!]!
+        }
+
+        type rootMutations {
+            createEvent(eventInput: EventInput): Event
         }
 
         schema {
             query: rootQuery
-            mutation: rootMutation
+            mutation: rootMutations
         }
     `),
     rootValue: {
-      posts: () => {
-        return [
-          "My first post",
-          "Arsenal will win the CL in 2024",
-          "I believe in JavaScript supremacy",
-        ];
+      events: () => {
+        return events;
       },
-      createPosts: (args) => {
-        const name = args.postName;
-        return name;
+      createEvent: (args) => {
+        const event = {
+          _id: Math.random().toString(),
+          name: args.eventInput.name,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date,
+        };
+        events.push(event);
+        console.log(event);
+        return event;
       },
     },
     graphiql: true,
   })
 );
+
+// app.use(
+//   "/graphql",
+//   graphqlHTTP({
+//     schema: buildSchema(`
+
+//         type rootQuery {
+//             posts: [String!]!
+//         }
+
+//         type rootMutation {
+//             createPosts(postName: String): String
+//         }
+
+//         schema {
+//             query: rootQuery
+//             mutation: rootMutation
+//         }
+//     `),
+//     rootValue: {
+//       posts: () => {
+//         return [
+//           "My first post",
+//           "Arsenal will win the CL in 2024",
+//           "I believe in JavaScript supremacy",
+//         ];
+//       },
+//       createPosts: (args) => {
+//         const name = args.postName;
+//         return name;
+//       },
+//     },
+//     graphiql: true,
+//   })
+// );
 
 app.listen(3000);
